f54a32994968bb0f63cf7847a0825fd07377a312
Thomas Pesendorfer <thomas.pesendorfer@urbangames.com>
2025-04-13 14:32:17 +0200

    added JS for all APIs

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -106,7 +106,6 @@ jQuery(document).ready(function($) {
         });
     });
     
-    // Similar AJAX calls for YouTube, Steam, Facebook, and X testing buttons:
     $('#sp-test-youtube').on('click', function() {
         $('#sp-youtube-test-result').html('Testing...');
         $.ajax({
@@ -129,7 +128,74 @@ jQuery(document).ready(function($) {
         });
     });
     
-    // Add similar code for the other buttons (#sp-test-steam, #sp-test-facebook, #sp-test-x)...
+    $('#sp-test-steam').on('click', function() {
+        $('#sp-steam-test-result').html('Testing...');
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            dataType: 'json',
+            data: { action: 'SOCPUL_test_steam_api' },
+            success: function(response) {
+                $('#sp-steam-test-result').html(response.message);
+                if(response.last_fetch_time) {
+                    $('#sp-steam-last-fetch-time').html(response.last_fetch_time);
+                }
+                if(response.last_fetch_value) {
+                    $('#sp-steam-last-fetch-value').html(response.last_fetch_value);
+                }
+            },
+            error: function(xhr, status, error) {
+                $('#sp-steam-test-result').html('Error: ' + error);
+            }
+        });
+    });
+    
+    $('#sp-test-facebook').on('click', function() {
+        $('#sp-facebook-test-result').html('Testing...');
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            dataType: 'json',
+            data: { action: 'SOCPUL_test_facebook_api' },
+            success: function(response) {
+                $('#sp-facebook-test-result').html(response.message);
+                if(response.last_fetch_time) {
+                    $('#sp-facebook-last-fetch-time').html(response.last_fetch_time);
+                }
+                if(response.last_fetch_value) {
+                    $('#sp-facebook-last-fetch-value').html(response.last_fetch_value);
+                }
+            },
+            error: function(xhr, status, error) {
+                $('#sp-facebook-test-result').html('Error: ' + error);
+            }
+        });
+    });
+    
+    $('#sp-test-x').on('click', function() {
+        $('#sp-x-test-result').html('Testing...');
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            dataType: 'json',
+            data: { action: 'SOCPUL_test_x_api' },
+            success: function(response) {
+                $('#sp-x-test-result').html(response.message);
+                if(response.last_fetch_time) {
+                    $('#sp-x-last-fetch-time').html(response.last_fetch_time);
+                }
+                if(response.last_fetch_value) {
+                    $('#sp-x-last-fetch-value').html(response.last_fetch_value);
+                }
+                if(response.api_limit_count) {
+                    $('#sp-x-api-limit-count').html(response.api_limit_count);
+                }
+            },
+            error: function(xhr, status, error) {
+                $('#sp-x-test-result').html('Error: ' + error);
+            }
+        });
+    });
 });
 JS;
     
